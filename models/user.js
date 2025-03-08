3ec968c05bf0b7ed92ae522702a63f3c15fd4c97
vdnvarma <dedeepn23@gmail.com>
2025-03-08 12:15:07 +0530

    Fixed mongouri for Railway deployment

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
 require('dotenv').config();  // Load environment variables
+const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log("Connected to MongoDB Atlas"))
